bbcb3e86ca172914c087c654316eb43c7324ff6a
Ramon Caballero Villegas <ramon.caballeroiv@gmail.com>
2022-05-18 15:41:49 -0500

    Restructuré los scripts para generar las gráficas del problema. Agregé script para graficar los resultados de la integración numérica hecha en el código principal de C++.

--- Graficas_EquilibrioHidrostaticoPerfil.m
+++
@@ -1,1 +0,0 @@
-dataPath = 'C:\Users\Ramon\Documents\Uni\Hidrodinamica y Magnetohidrodinamica\EquilibrioHidrostatico\EQHS_Data\data\Temperature-C7.dat'
---
+++ Scripts_Graficas/plot_data_helper.m
@@ -0,0 +1,11 @@
+function plot_data_helper(x, y, x_label, y_label, fig_title, format, y_log_scale)
+  if (y_log_scale)
+    semilogy(x, y, format, "MarkerSize", 2, "LineWidth", 1)
+  else
+    plot(x, y, format, "MarkerSize", 2, "LineWidth", 1)
+  endif
+  grid on
+  xlabel(x_label, "FontSize",  14)
+  ylabel(y_label, "FontSize",  14)
+  title(fig_title, "FontSize", 16)
+end
