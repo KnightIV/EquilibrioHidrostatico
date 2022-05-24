2ee3e21cc1006f9c85c60b2e2520386f73bf3d09
Ramon Caballero Villegas <ramon.caballeroiv@gmail.com>
2022-05-24 17:53:56 -0500

    Terminé intro y primera explicación de códigos de Octave. Empezando con el reporte del código C++. Actualizé gráficas para que su letra sea más legible en el PDF final.

--- Scripts_Graficas/plot_data_helper.m
+++ Scripts_Graficas/plot_data_helper.m
@@ -5,7 +5,7 @@ function plot_data_helper(x, y, x_label, y_label, fig_title, format, y_log_scale
     plot(x, y, format, "MarkerSize", 2, "LineWidth", 1)
   endif
   grid on
-  xlabel(x_label, "FontSize",  14)
-  ylabel(y_label, "FontSize",  14)
-  title(fig_title, "FontSize", 16)
+  xlabel(x_label)
+  ylabel(y_label)
+  title(fig_title)
 end
