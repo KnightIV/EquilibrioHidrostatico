5244cd329f869e0031a20cfccfa08719d0c3c42f
Ramon Caballero Villegas <ramon.caballeroiv@gmail.com>
2022-07-27 00:48:14 -0500

    Successfully init altitude grid on device

--- EquilibrioHidrostaticoCUDA/eqhs.cu
+++ EquilibrioHidrostaticoCUDA/eqhs.cu
@@ -10,25 +10,33 @@ struct SimProps {
 	const double gridStep = 1'000;
 	const double z_start = 0;
 	const double z_end = 6e6;
+
+	inline long gridSize() {
+		return ((long)(z_end - z_start)) / (long)gridStep;
+	}
 };
 
 __global__ void initAltitudeGrid(const SimProps *p, double *z) {
-	printf("%f, [%f, %f]\n", p->gridStep, p->z_start, p->z_end);
+	int gid = (blockIdx.x * blockDim.x) + threadIdx.x;
+	z[gid] = p->gridStep * gid;
 }
 
 int main() {
 	SimProps p;
+
 	SimProps *d_p;
-	
+	double *d_z;
+
 	gpuErrCheck(cudaMalloc((void**) &d_p, sizeof(SimProps)));
 	gpuErrCheck(cudaMemcpy(d_p, &p, sizeof(SimProps), cudaMemcpyHostToDevice));
+	gpuErrCheck(cudaMalloc((void**) &d_z, p.gridSize() * sizeof(double)));
 	
-	dim3 grid(1);
-	dim3 block(1);
-	initAltitudeGrid << <grid, block >> > (d_p, nullptr);
-
+	dim3 block(WARP_SIZE * 16);
+	dim3 grid((p.gridSize() / block.x) + 1);
+	initAltitudeGrid << <grid, block >> > (d_p, d_z);
 	gpuErrCheck(cudaDeviceSynchronize());
 	
 	gpuErrCheck(cudaFree(d_p));
+	gpuErrCheck(cudaFree(d_z));
 	return 0;
 }
